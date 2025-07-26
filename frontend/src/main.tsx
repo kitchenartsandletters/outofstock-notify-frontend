d8924f6104bcd0fc53b8585df214a68dcfd8dbcc
kitchenartsandletters <gil@kitchenartsandletters.com>
2025-07-26 17:43:24 -0400

    troubleshooting dark mode persistence:

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+console.log('Theme at load:', localStorage.getItem('theme'));
+console.log('HTML classes at load:', document.documentElement.className);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
